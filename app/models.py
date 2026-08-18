144ee585d1440d80a4798ded962bdad0ef2c72b2
Imadeddine AKLOUF <akimadeddine@gmail.com>
2026-08-18 11:54:37 +0200

    Create the Task class with all its methods and attributs

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class Task:
+    def __init__(self, id, title, date_of_creation):
+        self.id = id
+        self.title = title
+        self.date_of_creation = date_of_creation
+        self.done = False
+
+    def mark_as_done(self):
+        self.done = True
+
+    def modify_title(self, new_title):
+        self.title = new_title
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'date_of_creation': self.date_of_creation,
+            'done': self.done
+        }
+
+    def from_dict(cls, data):
+        return cls(
+            id=data.get('id'),
+            title=data.get('title'),
+            date_of_creation=data.get('date_of_creation')
+        )
