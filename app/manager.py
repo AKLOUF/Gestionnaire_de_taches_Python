d86f340fd619aa6076020a0a8164d43afb82604b
Imadeddine AKLOUF <akimadeddine@gmail.com>
2026-08-18 13:16:12 +0200

    Some corrections of the TaskManager class in order to be able to use it correctly in the cli.py file

--- app/manager.py
+++ app/manager.py
@@ -18,21 +18,37 @@ class TaskManager:
                 return task
         return None
 
-    def finish_task(self, task_id):
-        task = self.get_task_by_id(task_id)
+    def finish_task(self, task_title):
+        task = self.get_task_by_title(task_title)
         if task:
             task.mark_as_done()
             return True
         return False
 
-    def modify_task(self, task_id, new_title):
-        task = self.get_task_by_id(task_id)
+    def modify_task(self, task_title, new_title):
+        task = self.get_task_by_title(task_title)
         if task:
             task.modify_title(new_title)
             return True
         return False
 
-    def filter_tasks(self, done=None):
+    def filter_tasks_by_done_status(self, done=None):
         if done is None:
             return self.tasks
-        return [task for task in self.tasks if task.done == done]
+        return [task for task in self.tasks if task.done.lower() == done.lower()]
+
+    def filter_tasks_by_priority(self, priority):
+        return [task for task in self.tasks if task.priority == priority]
+
+    def mark_task_as_done(self, task_title):
+        task = self.get_task_by_title(task_title)
+        if task:
+            task.mark_as_done()
+            return True
+        return False
+
+    def get_task_by_title(self, task_title):
+        for task in self.tasks:
+            if task.title == task_title:
+                return task
+        return None
