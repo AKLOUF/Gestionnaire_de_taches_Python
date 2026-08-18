823532770549ef35e2dc0358b4f0a7b52d0f4f90
Imadeddine AKLOUF <akimadeddine@gmail.com>
2026-08-18 12:31:12 +0200

    Creation of the 'save_task' and 'return_task' methods in the storage file with all the exceptions handlings

---
+++ app/storage.py
@@ -0,0 +1,33 @@
+import json
+from turtle import done
+from app.models import Task 
+
+def save_tasks(tasks, filename):
+    try:
+        with open(filename, 'w') as file:
+            json.dump([task.to_dict() for task in tasks], file)
+    except FileNotFoundError:
+        print(f"File {filename} not found. Returning an empty task list.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from {filename}. Returning an empty task list.")
+    except Exception as e:
+        print(f"An error occurred while reading {filename}: {e}. Returning an empty task list.")
+    return 0
+
+def return_tasks(filename):
+    tasks = []
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            for task_data in data:
+                task = Task.from_dict(task_data)
+                tasks.append(task)
+                task.done = done == 'True'
+                tasks.append(task)
+    except FileNotFoundError:
+        print(f"File {filename} not found. Returning an empty task list.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from {filename}. Returning an empty task list.")
+    except Exception as e:
+        print(f"An error occurred while reading {filename}: {e}. Returning an empty task list.")
+    return tasks
