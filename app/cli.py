e5c79693ee11ab389cef7267946b76addbf6f4f8
Imadeddine AKLOUF <akimadeddine@gmail.com>
2026-08-20 15:03:34 +0200

    Correction of the imports in cli.py

--- app/cli.py
+++ app/cli.py
@@ -2,7 +2,6 @@ from storage import save_tasks, return_tasks
 from manager import TaskManager
 from models import TaskPriority
 from models import TaskStatus
-from storage import save_tasks, return_tasks
 
 DATA_FILE = "../data/backup.json"
 
