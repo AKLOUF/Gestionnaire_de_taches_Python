f92e94735d53856baaf7cb139c229fa696f29bf6
Imadeddine AKLOUF <akimadeddine@gmail.com>
2026-08-21 11:06:28 +0200

    Correction of test_models.py file

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,7 @@ creation_date = "2023-10-01"
 priority = TaskPriority.HIGH.value
 description = "This is a test task."
 status = TaskStatus.NOT_STARTED.value
-
+new_title = "Modified Task Title"
 
 task = Task(id, title, creation_date, priority, description, status)
 
@@ -16,7 +16,7 @@ def test_mark_task_as_done():
     assert task.status == TaskStatus.DONE.value
 
 def test_modify_task_title():
-    new_title = "Modified Task Title"
+
     task.modify_title(new_title)
     assert task.title == new_title
 
@@ -24,27 +24,27 @@ def test_modify_task_title():
 def test_task_to_dict():
     task_dict = task.to_dict()
     assert task_dict['id'] == id
-    assert task_dict['title'] == title
+    assert task_dict['title'] == new_title
     assert task_dict['creation_date'] == creation_date
     assert task_dict['priority'] == priority
     assert task_dict['description'] == description
-    assert task_dict['status'] == status
+    assert task_dict['status'] == TaskStatus.DONE.value
 
 def test_task_from_dict():
     task_data = {
         'id': id,
-        'title': title,
+        'title': new_title,
         'creation_date': creation_date,
         'priority': priority,
         'description': description,
-        'status': status
+        'status': TaskStatus.DONE.value
     }
     new_task = Task.from_dict(task_data)
     assert new_task.id == id
-    assert new_task.title == title
+    assert new_task.title == new_title
     assert new_task.creation_date == creation_date
     assert new_task.priority == priority
     assert new_task.description == description
-    assert new_task.status == status
+    assert new_task.status == TaskStatus.DONE.value
 
 
